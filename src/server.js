f67d9cb9ec227209399aa478e8a7bb67a901b155
Bruno Krebs <bruno.krebs@auth0.com>
2019-04-03 21:41:31 -0300

    refactoring mongoose integration;

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const { insertProduct, getProducts } = require('./database/products');
-const { deleteProduct, updateProduct } = require('./database/products');
+const Product = require('./database/product.model');
 
 // defining the Express app
 const app = express();
@@ -17,25 +16,31 @@ app.use(morgan('combined'));
 
 // endpoint to return all ads
 app.get('/', async (req, res) => {
-  res.send(await getProducts());
+  res.send(await Product.find({}));
 });
 
 app.post('/', async (req, res) => {
-  const newProduct = req.body;
-  await insertProduct(newProduct);
+  const newProduct = new Product(req.body);
+  await newProduct.save();
   res.send({ message: 'New product inserted.' });
 });
 
 // endpoint to delete a product
 app.delete('/:id', async (req, res) => {
-  await deleteProduct(req.params.id);
+  await Product.deleteOne({ _id: req.params.id });
   res.send({ message: 'Product removed.' });
 });
 
 // endpoint to update a product
 app.put('/:id', async (req, res) => {
-  const updatedProduct = req.body;
-  await updateProduct(req.params.id, updatedProduct);
+  await Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        ...req.body
+      }
+    }
+  );
   res.send({ message: 'Product updated.' });
 });
 
